b0a45155c15a5ddfe1e5976d2a5ca6cf2ba0398f
yangyniii <yani.yang@ucdconnect.ie>
2024-05-14 16:17:54 +0800

    update test

--- src/main/java/com/example/test/HelloApplication.java
+++ src/main/java/com/example/test/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
