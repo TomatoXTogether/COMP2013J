c11c87784963e3f4510d6b7a4020a0a2de7919d7
yangyniii <yani.yang@ucdconnect.ie>
2024-05-16 11:20:52 +0800

    Update StudentDAO.java and Student.java

--- src/main/java/com/example/test/StudentDAO.java
+++ src/main/java/com/example/test/StudentDAO.java
@@ -10,12 +10,13 @@ import java.util.List;
 public class StudentDAO {
     public static List<Student> getAllStudents() {
         List<Student> employees = new ArrayList<Student>();
+        List<Lecture> lectures = new ArrayList<Lecture>();
 
         try {
             Connection conn = JDBCTool.getConnection();
             Statement st = conn.createStatement();
 
-            ResultSet rs = st.executeQuery("SELECT * FROM students");
+            ResultSet rs = st.executeQuery("SELECT DISTINCT studentID, name, email, password FROM students");
 
             for (int i = 0; i < 6; i ++) {
                 int studentID = rs.getInt("studentID");
@@ -23,7 +24,7 @@ public class StudentDAO {
                 String email = rs.getString("email");
                 int password = rs.getInt(" password");
 
-                Student e = new Student(studentID, name, password, email);
+                Student e = new Student(studentID, name, password, email, lectures);
 
                 employees.add(e);
             }
