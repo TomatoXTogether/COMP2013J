0fbdaecbcab39a4f2e9d95861d75ca4d36a52228
yangyniii <yani.yang@ucdconnect.ie>
2024-05-20 19:16:11 +0800

    Correct type of lectureID

--- src/main/java/com/example/test/LectureCheck.java
+++ src/main/java/com/example/test/LectureCheck.java
@@ -6,7 +6,7 @@ import com.example.test.Checkbox;
 
 public class LectureCheck extends Lecture implements Serializable {
 
-        public LectureCheck(int lectureID, int lecturerID, String name, int room, int building, String schedule, String startDate, String endDate) {
+        public LectureCheck(String lectureID, int lecturerID, String name, int room, int building, String schedule, String startDate, String endDate) {
             super(lectureID, lecturerID, name, room, building, schedule, startDate, endDate);
             // TODO Auto-generated constructor stub
         }
