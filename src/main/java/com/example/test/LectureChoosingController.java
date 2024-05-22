1ded92dafd2b985a22b1fd7fbe187418c395b5ef
Wenyi <16601151855@163.com>
2024-05-22 17:06:43 +0800

    README.md

--- src/main/java/com/example/test/LectureChoosingController.java
+++ src/main/java/com/example/test/LectureChoosingController.java
@@ -64,6 +64,9 @@ public class LectureChoosingController implements Initializable {
     private Button save;
 
     @FXML
+    private Button delete;
+
+    @FXML
     private TableColumn<Lecture, String> schedule;
 
     @FXML
@@ -134,9 +137,9 @@ public class LectureChoosingController implements Initializable {
         selectedLectures = lecturesData.stream()
                 .filter(Lecture::isSelected)
                 .collect(Collectors.toList());
-        System.out.println(selectedLectures.size());
+        //System.out.println(selectedLectures.size());
         List<Lecture> updatedLectures = LectureDAO.getAllLectures();
-        System.out.println(selectedLectures.size());
+        //System.out.println(selectedLectures.size());
         for (Lecture lecture : lecturesData) {
             // 更新课程对象的属性，保持勾选状态不变
             updatedLectures.stream()
@@ -144,7 +147,7 @@ public class LectureChoosingController implements Initializable {
                     .findFirst().ifPresent(updatedLecture -> updatedLecture.setSelected(lecture.isSelected()));
         }
         lecturesData.setAll(updatedLectures);
-        System.out.println(selectedLectures.size());
+        //System.out.println(selectedLectures.size());
         LectureTable.setItems(lecturesData);
     }
 
@@ -164,6 +167,26 @@ public class LectureChoosingController implements Initializable {
                 // 在此处添加保存逻辑//
                 StudentDAO.selectLecture(userInfo, lecture);
                 StudentDAO.getStudentByID(userInfo.getID()).lectures.add(lecture);
+            }
+        } else {
+            System.out.println("No lectures selected.");
+        }
+        return lectures;
+    }
+    @FXML
+    List<Lecture> deleteBottonAction(ActionEvent event) {
+        selectedLectures = lecturesData.stream()
+                .filter(Lecture::isSelected)
+                .peek(lecture -> lecture.setSelected(true))
+                .collect(Collectors.toList());
+
+        List<Lecture> lectures = new ArrayList<Lecture>();
+
+        if (!selectedLectures.isEmpty()) {
+            for (Lecture lecture : selectedLectures) {
+                // 在此处添加保存逻辑//
+                StudentDAO.cancelLecture(userInfo, lecture);
+                //StudentDAO.getStudentByID(userInfo.getID()).lectures.remove(lecture);
             }
         } else {
             System.out.println("No lectures selected.");
@@ -194,8 +217,6 @@ public class LectureChoosingController implements Initializable {
 
             TableColumn checkBoxColumn = new TableColumn("");
             checkBox.setCellFactory(CheckBoxTableCell.forTableColumn(checkBoxColumn));
-
-
 
             checkBox.setCellValueFactory(cellData -> {
                 Lecture lecture = cellData.getValue();
